f60142270c8e7d182658e3a06f855242bbbe5dcd
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-02-01 17:30:16 +0530

    adding node projects loike node calculator and how to use node and npm.

---
+++ intro-to-node/index.js
@@ -0,0 +1,9 @@
+var superheroes = require('superheroes');
+
+var supername = superheroes.random()
+
+console.log(supername);
+
+var supervillains = require('supervillains');
+var supervill = supervillains.random();
+console.log(supervill);
