d57add807692f1f768163146c23f45cd444f1c5f
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-02-01 18:45:12 +0530

    add update js file

--- node-Claculator/calculator.js
+++ node-Claculator/calculator.js
@@ -10,7 +10,7 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
     console.log(req.body);
-    console.log(req.body.num1)
+    console.log(req.body.num1);
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
     
@@ -19,6 +19,21 @@ app.post("/", function(req, res){
     res.send("thanks for yout response we will keep that in mind.Your answer is " + result);
 })
 
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmicalculator.html");
+})
+app.post("/bmicalculator", function(req, res){
+    console.log(req.body);
+    console.log(req.body.weight);
+    console.log(req.body.height);
+
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+
+    var bmi = weight / (height * height);
+    console.log(bmi);
+    res.send("Your BMI is" + bmi);
+})
 app.listen(3000, function () {
     console.log("Serever Started at rout 3000."); 
 })
