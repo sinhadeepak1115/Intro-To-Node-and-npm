5154142b51be0c6680f2578214c5f0025d7f3878
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-02-02 19:59:12 +0530

    complete Weather app.

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,38 +1,43 @@
 const { response } = require('express');
 const express = require('express');
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}))
 
 const http = require('http');
 
-    app.get('/', function (req, res){
-        res.sendFile(__dirname + "/index.html")        
-    })
-    app.post("/", function(req, res){
-        console.log("Post request recived")
-    })
-// const City = "Patna";
-// const appid = "31f2772c8481253024098bda0bad3b76";
-// const units = "metric";
+app.get('/', function (req, res){
+    res.sendFile(__dirname + "/index.html")        
+})
+app.post("/", function(req, res){
+const City = req.body.cityInput;
+const appid = "31f2772c8481253024098bda0bad3b76";
+const units = "metric";
 
-// const url = "http://api.openweathermap.org/data/2.5/weather?q="+ City +"&appid=" +appid +"&units=" + units;
-// http.get(url, function(responce) {
-//     console.log(responce.statusCode);
-//     responce.on('data', function(data) {
-//         var weatherData = JSON.parse(data);
-//         console.log(weatherData);
-//         const weatherTemp = weatherData.main.temp;
-//         console.log(weatherTemp);
-//         const weatherFeelsLike = weatherData.weather[0].description;
-//         console.log(weatherFeelsLike);
-//         const iconId = weatherData.weather[0].icon;
-//         console.log(iconId);
-//         const urlWeatherIcon  = "http://openweathermap.org/img/wn/"+ iconId +"@2x.png"
-//         res.write("<h1>The Temperature in "+ City +" is "+ weatherTemp +" degrees celsius</h1>");
-//         res.write("<h3>It appera to be "+ weatherFeelsLike + " outside</h3>")
-//         res.write("<img src=" + urlWeatherIcon+ ">");
-//         res.send();
-//     })
-// })
+const url = "http://api.openweathermap.org/data/2.5/weather?q="+ City +"&appid=" +appid +"&units=" + units;
+http.get(url, function(responce) {
+    console.log(responce.statusCode);
+    responce.on('data', function(data) {
+        var weatherData = JSON.parse(data);
+        console.log(weatherData);
+        const weatherTemp = weatherData.main.temp;
+        console.log(weatherTemp);
+        const weatherFeelsLike = weatherData.weather[0].description;
+        console.log(weatherFeelsLike);
+        const iconId = weatherData.weather[0].icon;
+        console.log(iconId);
+        const urlWeatherIcon  = "http://openweathermap.org/img/wn/"+ iconId +"@2x.png"
+        res.write("<h1>The Temperature in "+ City +" is "+ weatherTemp +" degrees celsius</h1>");
+        res.write("<h3>It appera to be "+ weatherFeelsLike + " outside</h3>")
+        res.write("<img src=" + urlWeatherIcon+ ">");
+        res.send();
+    })
+})
+
+    })
+
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000 plz check.")
